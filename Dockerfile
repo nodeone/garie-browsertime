bef36a4d8215b5ebd003ec0fc49191c2643f98f1
mikkeschiren <mikke.schiren@digitalistgroup.com>
2019-01-10 14:10:50 +0100

    slimmer image

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,7 @@
-FROM node:8.10.0
+FROM node:10-slim
+
+RUN apt update \
+ && apt install libltdl-dev -y
 
 RUN mkdir -p /usr/src/garie-browsertime
 RUN mkdir -p /usr/src/garie-browsertime/reports
@@ -12,5 +15,9 @@ RUN npm install
 COPY . .
 
 EXPOSE 3000
+
+VOLUME ["/usr/src/garie-browsertime/reports", "/usr/src/garie-lighthouse/logs"]
+
+ENTRYPOINT ["/usr/src/garie-browsertime/docker-entrypoint.sh"]
 
 CMD ["npm", "start"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,19 +1,10 @@
 #!/bin/sh
 set -e
 
-if [ "${1:0:1}" = '-' ]; then
-	set -- docker "$@"
-fi
 
-# if our command is a valid Docker subcommand, let's invoke it through Docker instead
-# (this allows for "docker run docker ps", etc)
-if docker "$1" --help > /dev/null 2>&1; then
-	set -- docker "$@"
-fi
-
-# if we have "--link some-docker:docker" and not DOCKER_HOST, let's set DOCKER_HOST automatically
-if [ -z "$DOCKER_HOST" -a "$DOCKER_PORT_2375_TCP" ]; then
-	export DOCKER_HOST='tcp://docker:2375'
+if [ -n "$CONFIG" ]; then
+	echo "Found configuration variable, will write it to the /usr/src/garie-browsertime/config.json"
+	echo "$CONFIG" > /usr/src/garie-browsertime/config.json
 fi
 
 exec "$@"
